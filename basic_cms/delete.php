a8467e0724c9d32eddf906d8f9b3ff879a1ccc51
fahad611 <you@example.com>
2017-04-17 23:34:45 -0700

    Basic content management system

--- basic_cms/delete.php
+++ basic_cms/delete.php
@@ -1,22 +1,23 @@
 <?php
+include ("include/db.php");
 
-include("include/db.php");
-
-
-if (isset($_GET['del_page'])) 
+if (isset($_GET['del_page']))
 {
- $delete_page = $_GET['del_page'];
- $query = "DELETE FROM pages where p_id='$delete_page'";
+	$delete_page = $_GET['del_page'];
+	$query = "DELETE FROM pages where p_id='$delete_page'";
 }
-if (isset($_GET['del_menu'])) 
+
+if (isset($_GET['del_menu']))
 {
- $delete_menu = $_GET['del_menu'];
- $query = "DELETE FROM menus where m_id='$delete_menu'";
+	$delete_menu = $_GET['del_menu'];
+	$query = "DELETE FROM menus where m_id='$delete_menu'";
 }
 
 if (mysqli_query($con, $query))
 {
- echo "<script>window.open('admin_panel.php?deleted = data has been deleted...','_self')</script>";
+	echo "<script>window.open('admin_panel.php?deleted = data has been deleted...','_self')</script>";
 }
 
 ?>
+
+ 
